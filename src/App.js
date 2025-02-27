6ea63915557996804dfe35106440a73170b8c15a
seo7905 <wjddks64@gmail.com>
2025-02-27 16:11:11 +0900

    React Mui 사용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './pages/Header';
+import Nav from './pages/Nav';
+import Content from './pages/Content';
+import { Container, Grid2 } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container fixed>  
+        <Header/>
+        <Grid2 container>
+          <Grid2 size={3}><Nav/></Grid2>
+          <Grid2 size={9}><Content/></Grid2>
+        </Grid2>
+      </Container>
   );
 }
 
---
+++ src/pages/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Nav(){
+    return(
+        <nav>
+            <ol>
+                <li>메뉴1</li>&nbsp;&nbsp;&nbsp;
+                <li>메뉴2</li>&nbsp;&nbsp;&nbsp;
+                <li>메뉴3</li>&nbsp;&nbsp;&nbsp;
+            </ol>
+        </nav>
+    );
+}
